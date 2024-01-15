1e103b41bdbe95f3d6674876d9c0a1354f8856dd
Miee7 <152489745+Miee7@users.noreply.github.com>
2024-01-16 00:40:31 +0800

    Update mb.py

--- mb.py
+++ mb.py
@@ -1,39 +1,71 @@
 import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 import pandas as pd
-import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from statistics import mode
 
-# Load the data
-df = pd.read_csv("Employee.csv")
 
-# Preprocessing
-df.columns = df.columns.str.lower()
-df['gender'] = df['gender'].map({'Male': 1, 'Female': 0})
-df['everbenched'] = df['everbenched'].map({'Yes': 1, 'No': 0})
-df['education'] = df['education'].map({'Bachelors': 0, 'Masters': 1, 'PHD': 2})
-df = pd.get_dummies(df, columns=['city'])
 
-scaler = MinMaxScaler()
-scaler.fit(df[['joiningyear', 'paymenttier', 'experienceincurrentdomain']])
-df[['joiningyear', 'paymenttier', 'experienceincurrentdomain']] = scaler.transform(
-    df[['joiningyear', 'paymenttier', 'experienceincurrentdomain']]
-)
 
-# Split the data into features and labels
-features = df.drop(['leaveornot'], axis=1)
-labels = df['leaveornot']
+#Initialize the flask App
+app = Flask(__name__)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+model = pickle.load(open('models/model_lr.pkl', 'rb'))
+model1 = pickle.load(open('models/model_rf.pkl', 'rb'))
+model2 = tf.keras.models.load_model("models/model") # It can be used to reconstruct the model identically.
 
-# Create a RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-classifier.fit(X_train, y_train)
+X_train = pickle.load(open('models/X_train.pkl','rb'))
 
-# Streamlit App
-def main():
-    st
+
+scx = pickle.load(open('models/scx.pkl','rb')) #minmax scaler
+onec = pickle.load(open('models/onec.pkl','rb'))
+X_train = pickle.load(open('models/X_train.pkl','rb'))
+X_train_cat = pickle.load(open('models/X_train_cat.pkl','rb'))
+y_train = pickle.load(open('models/y_train.pkl','rb'))
+
+#default page of our web-app
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#To use the predict button in our web-app
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [x for x in request.form.values()]
+    cat_features = features[0:5]
+    num_features = features[5:10]
+    X_test = {'JobRole':[cat_features[0]],'Department':[cat_features[1]],'BusinessTravel':[cat_features[2]],'EducationField':[cat_features[3]],'MaritalStatus':[cat_features[4]]}
+    X_test = pd.DataFrame(X_test)
+    onec_x = onec.fit(X_train_cat)  #fitting the encoder object onto the train dataset
+    X_test = onec.transform(X_test) #converting the categorical features intop ordinal values based on the fit on training dataset
+    X_test = pd.DataFrame(X_test)
+    col_one_list = X_test.values.tolist()
+    cat_features = col_one_list[0]
+    final_features = [*cat_features,*num_features] #combining two lists 
+    
+    final_features = [np.array(final_features)]
+    x = scx.fit(X_train)
+    final_features = scx.transform(final_features)
+    prediction = model.predict(final_features)
+    prediction_1 = model1.predict(final_features)
+    prediction_2 = model2.predict(final_features)
+    output = prediction.tolist()
+    output1 = prediction_1.tolist()
+    output2 = prediction_2.tolist()
+    ls = [output[0],output1[0],int(output2[0][0])]
+    
+    result = mode(ls) # Returns the highest occurring item
+    if result == 1:
+        rs = "Employee is likely to leave"
+    else:
+        rs = "Employee is not likely to leave"
+
+    return render_template('index.html', prediction_text=rs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
