cd9a60576fc1a690345a27c3f50f1c1222e1b77e
Miee7 <152489745+Miee7@users.noreply.github.com>
2024-01-16 00:50:00 +0800

    Update mb.py

--- mb.py
+++ mb.py
@@ -1,71 +1,63 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
 import pandas as pd
-import tensorflow as tf
-from statistics import mode
+import streamlit as st 
+from sklearn import preprocessing
+import pickle
 
-
-
-
-#Initialize the flask App
-app = Flask(__name__)
-
-model = pickle.load(open('models/model_lr.pkl', 'rb'))
-model1 = pickle.load(open('models/model_rf.pkl', 'rb'))
-model2 = tf.keras.models.load_model("models/model") # It can be used to reconstruct the model identically.
-
-X_train = pickle.load(open('models/X_train.pkl','rb'))
-
-
-scx = pickle.load(open('models/scx.pkl','rb')) #minmax scaler
-onec = pickle.load(open('models/onec.pkl','rb'))
-X_train = pickle.load(open('models/X_train.pkl','rb'))
-X_train_cat = pickle.load(open('models/X_train_cat.pkl','rb'))
-y_train = pickle.load(open('models/y_train.pkl','rb'))
-
-#default page of our web-app
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-#To use the predict button in our web-app
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    features = [x for x in request.form.values()]
-    cat_features = features[0:5]
-    num_features = features[5:10]
-    X_test = {'JobRole':[cat_features[0]],'Department':[cat_features[1]],'BusinessTravel':[cat_features[2]],'EducationField':[cat_features[3]],'MaritalStatus':[cat_features[4]]}
-    X_test = pd.DataFrame(X_test)
-    onec_x = onec.fit(X_train_cat)  #fitting the encoder object onto the train dataset
-    X_test = onec.transform(X_test) #converting the categorical features intop ordinal values based on the fit on training dataset
-    X_test = pd.DataFrame(X_test)
-    col_one_list = X_test.values.tolist()
-    cat_features = col_one_list[0]
-    final_features = [*cat_features,*num_features] #combining two lists 
+model = pickle.load(open('model.pkl', 'rb'))
+encoder_dict = pickle.load(open('encoder.pkl', 'rb')) 
+cols=['age','workclass','education','marital-status','occupation','relationship','race','gender','capital-gain','capital-loss',
+      'hours-per-week','native-country']    
+  
+def main(): 
+    st.title("Income Predictor")
+    html_temp = """
+    <div style="background:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Income Prediction App </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html = True)
     
-    final_features = [np.array(final_features)]
-    x = scx.fit(X_train)
-    final_features = scx.transform(final_features)
-    prediction = model.predict(final_features)
-    prediction_1 = model1.predict(final_features)
-    prediction_2 = model2.predict(final_features)
-    output = prediction.tolist()
-    output1 = prediction_1.tolist()
-    output2 = prediction_2.tolist()
-    ls = [output[0],output1[0],int(output2[0][0])]
+    age = st.text_input("Age","0") 
+    workclass = st.selectbox("Working Class", ["Federal-gov","Local-gov","Never-worked","Private","Self-emp-inc","Self-emp-not-inc","State-gov","Without-pay"]) 
+    education = st.selectbox("Education",["10th","11th","12th","1st-4th","5th-6th","7th-8th","9th","Assoc-acdm","Assoc-voc","Bachelors","Doctorate","HS-grad","Masters","Preschool","Prof-school","Some-college"]) 
+    marital_status = st.selectbox("Marital Status",["Divorced","Married-AF-spouse","Married-civ-spouse","Married-spouse-absent","Never-married","Separated","Widowed"]) 
+    occupation = st.selectbox("Occupation",["Adm-clerical","Armed-Forces","Craft-repair","Exec-managerial","Farming-fishing","Handlers-cleaners","Machine-op-inspct","Other-service","Priv-house-serv","Prof-specialty","Protective-serv","Sales","Tech-support","Transport-moving"]) 
+    relationship = st.selectbox("Relationship",["Husband","Not-in-family","Other-relative","Own-child","Unmarried","Wife"]) 
+    race = st.selectbox("Race",["Amer Indian Eskimo","Asian Pac Islander","Black","Other","White"]) 
+    gender = st.selectbox("Gender",["Female","Male"]) 
+    capital_gain = st.text_input("Capital Gain","0") 
+    capital_loss = st.text_input("Capital Loss","0") 
+    hours_per_week = st.text_input("Hours per week","0") 
+    nativecountry = st.selectbox("Native Country",["Cambodia","Canada","China","Columbia","Cuba","Dominican Republic","Ecuador","El Salvadorr","England","France","Germany","Greece","Guatemala","Haiti","Netherlands","Honduras","HongKong","Hungary","India","Iran","Ireland","Italy","Jamaica","Japan","Laos","Mexico","Nicaragua","Outlying-US(Guam-USVI-etc)","Peru","Philippines","Poland","Portugal","Puerto-Rico","Scotland","South","Taiwan","Thailand","Trinadad&Tobago","United States","Vietnam","Yugoslavia"]) 
     
-    result = mode(ls) # Returns the highest occurring item
-    if result == 1:
-        rs = "Employee is likely to leave"
-    else:
-        rs = "Employee is not likely to leave"
+    if st.button("Predict"): 
+        features = [[age,workclass,education,marital_status,occupation,relationship,race,gender,capital_gain,capital_loss,hours_per_week,nativecountry]]
+        data = {'age': int(age), 'workclass': workclass, 'education': education, 'maritalstatus': marital_status, 'occupation': occupation, 'relationship': relationship, 'race': race, 'gender': gender, 'capitalgain': int(capital_gain), 'capitalloss': int(capital_loss), 'hoursperweek': int(hours_per_week), 'nativecountry': nativecountry}
+        print(data)
+        df=pd.DataFrame([list(data.values())], columns=['age','workclass','education','maritalstatus','occupation','relationship','race','gender','capitalgain','capitalloss','hoursperweek','nativecountry'])
+                
+        category_col =['workclass', 'education', 'maritalstatus', 'occupation', 'relationship', 'race', 'gender', 'nativecountry']
+        for cat in encoder_dict:
+            for col in df.columns:
+                le = preprocessing.LabelEncoder()
+                if cat == col:
+                    le.classes_ = encoder_dict[cat]
+                    for unique_item in df[col].unique():
+                        if unique_item not in le.classes_:
+                            df[col] = ['Unknown' if x == unique_item else x for x in df[col]]
+                    df[col] = le.transform(df[col])
+            
+        features_list = df.values.tolist()      
+        prediction = model.predict(features_list)
+    
+        output = int(prediction[0])
+        if output == 1:
+            text = ">50K"
+        else:
+            text = "<=50K"
 
-    return render_template('index.html', prediction_text=rs)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        st.success('Employee Income is {}'.format(text))
+      
+if __name__=='__main__': 
+    main()
