42b5399af99283c8039461887f2d571119d2345a
Miee7 <152489745+Miee7@users.noreply.github.com>
2024-01-17 21:13:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,3 +63,6 @@ def main():
         # Display prediction
         st.subheader('Prediction:')
         st.write('Leave: Yes' if prediction[0] == 1 else 'Leave: No')
+        
+if __name__ == '__main__':
+    main()
