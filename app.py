e4e5f34b50e470c1f48391052a757ba15adbc7db
Miee7 <152489745+Miee7@users.noreply.github.com>
2024-01-17 19:13:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def main():
         user_df = pd.get_dummies(user_df, columns=categorical_cols)
 
         # Ensure that the columns in user_df match the columns used during model training
-        expected_columns = ['education', 'joiningyear', 'city', 'paymenttier', 'age', 'gender', 'everbenched',
+        expected_columns = ['education', 'joiningyear', 'city_Bangalore', 'city_New Delhi', 'city_Pune', 'paymenttier', 'age', 'gender', 'everbenched',
                             'experienceincurrentdomain']
         
         # Align columns in user_df
