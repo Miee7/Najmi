ce176692a4d299427a4edc240c395f8b48c0c90e
Miee7 <152489745+Miee7@users.noreply.github.com>
2024-01-17 22:01:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ def main():
     st.title('FUTURE EMPLOYEE ATTRITION')
     html_temp = """
     <div style="background:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">NAJMI & MAS</h2>
+    <h2 style="color:white;text-align:center;">Income Prediction App </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
     # User input form
     education = st.selectbox('Education:', ['Bachelor', 'Master'])
-    joiningyear = st.slider('JoiningYYear:', 2012, 2018, 2015)
+    joining_year = st.slider('Joining Year:', 2012, 2018, 2015)
     city = st.selectbox('City:', ['Bangalore', 'Pune', 'New Delhi'])
-    paymenttier = st.selectbox('PaymentTier:', [1, 2, 3])
+    payment_tier = st.selectbox('Payment Tier:', [1, 2, 3])
     age = st.text_input("Age", "0")
     gender = st.selectbox('Gender:', ['Male', 'Female'])
-    everbenched = st.selectbox('EverBenched:', ['Yes', 'No'])
-    experienceincurrentdomain = st.number_input('ExperienceInCurrentDomain:')
+    ever_benched = st.selectbox('Ever Benched:', ['Yes', 'No'])
+    experience_in_current_domain = st.number_input('Experience in Current Domain:')
 
     user_input_form = st.form('user_input_form')
 
@@ -34,15 +34,15 @@ def main():
     if user_input_form.form_submit_button('Predict'):
         # Create DataFrame from user input
         user_data = {
-            'Education': [education],
-            'JoiningYear': [joiningyear],
-            'City': [city],
-            'PaymentTier': [paymenttier],
-            'Age': [age],
-            'Gender': [gender],
-            'EverBenched': [everbenched],
-            'ExperienceInCurrentDomain': [experienceincurrentdomain],
-    }
+            'Education': [Education],
+            'JoiningYear': [JoiningYear],
+            'City': [City],
+            'PaymentTier': [PaymentTier],
+            'Age': [Age],
+            'Gender': [Gender],
+            'EverBenched': [EverBenched],
+            'ExperienceInCurrentDomain': [ExperienceInCurrentDomain],
+        }
 
         user_df = pd.DataFrame(user_data)
 
@@ -51,8 +51,8 @@ def main():
         user_df = pd.get_dummies(user_df, columns=categorical_cols)
 
         # Ensure that the columns in user_df match the columns used during model training
-        expected_columns = ['education', 'joiningyear', 'city_Bangalore', 'city_Pune', 'city_New Delhi', 'paymenttier', 'age', 'gender', 'everbenched', 'experienceincurrentdomain']
-
+        expected_columns = ['education', 'joiningyear', 'city_Bangalore', 'city_New Delhi', 'city_Pune', 'paymenttier', 'age', 'gender', 'everbenched',
+                    'experienceincurrentdomain']
         
         # Align columns in user_df
         user_df = user_df.reindex(columns=expected_columns, fill_value=0)
@@ -63,6 +63,6 @@ def main():
         # Display prediction
         st.subheader('Prediction:')
         st.write('Leave: Yes' if prediction[0] == 1 else 'Leave: No')
-        
-if __name__ == '__main__':
-    main()
+
+if _name=='main_': 
+    main()
